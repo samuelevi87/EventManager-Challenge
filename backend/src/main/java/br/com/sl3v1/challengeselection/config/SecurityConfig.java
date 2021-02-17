e39ddaed0e3676ee3d4790fe6fb65136dc1b2110
samuelevi87 <samuel.levi.alves@outlook.com>
2021-02-17 15:59:09 -0300

    Profiles setup and security configuration

---
+++ backend/src/main/java/br/com/sl3v1/challengeselection/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package br.com.sl3v1.challengeselection.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {//liberando o acesso da aplicação ao banco H2
+			http.headers().frameOptions().disable();
+		}
+		
+		http.cors().and().csrf().disable();//liberando o CORS, desabilitando a proteção de ataque CSRF (ataque baseado em sessão), a aplicação será REST, por isso não sofre esse tipo de ataque.
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//Não armazena o estado.
+		http.authorizeRequests().anyRequest().permitAll();//Permitindo o acesso à todas requisições.
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
