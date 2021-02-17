51d84911346894fd000458872e1039d5105c69d0
samuelevi87 <samuel.levi.alves@outlook.com>
2021-02-17 14:24:12 -0300

    Project created

---
+++ backend/src/main/java/br/com/sl3v1/challengeselection/ChallengeSelectionApplication.java
@@ -0,0 +1,13 @@
+package br.com.sl3v1.challengeselection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeSelectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeSelectionApplication.class, args);
+	}
+
+}
