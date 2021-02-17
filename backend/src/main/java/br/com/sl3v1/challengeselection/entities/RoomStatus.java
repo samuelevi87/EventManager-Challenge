c3178c287909104eb553457c7e63122f2d3507f1
samuelevi87 <samuel.levi.alves@outlook.com>
2021-02-17 19:52:18 -0300

    First classes

---
+++ backend/src/main/java/br/com/sl3v1/challengeselection/entities/RoomStatus.java
@@ -0,0 +1,6 @@
+package br.com.sl3v1.challengeselection.entities;
+
+public enum RoomStatus {
+
+	EMPTY, AVAILABLE, FULL;
+}
---
+++ backend/src/main/java/br/com/sl3v1/challengeselection/entities/Stage.java
@@ -0,0 +1,5 @@
+package br.com.sl3v1.challengeselection.entities;
+
+public enum Stage {
+	STAGE1, STAGE2;
+}
